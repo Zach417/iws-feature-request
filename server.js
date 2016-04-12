9750a56c2202f5e40e5f18a05be3a241b854af13
Zach Allen <zallen@pension-consultants.com>
2016-04-11 22:16:14 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(function (req, res, next) {
+  console.log(req.method + req.path);
+	next();
+});
+
+app.use('/', require('./server/router'));
+
+app.listen(80);
+console.log('Magic happens on port 80');
---
+++ server/router/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+router.use('/favicon.ico', require('./favicon'));
+router.use('/js', require('./js'));
+router.use('/css', require('./css'));
+router.use('/', require('./views'));
+
+module.exports = router;
