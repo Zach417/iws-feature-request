e18057266b9438f037f7e7a9e4b0f908b484fb70
Zach417 <zallen@pension-consultants.com>
2016-04-12 19:49:09 -0500

    Configuring feature store route

--- server/router/stores/feature.js
+++ server/router/stores/feature.js
@@ -1,10 +1,55 @@
+var moment = require('moment');
 var express = require('express');
 var router = express.Router();
 var Feature = require('../../models/feature');
 
+function isValidInt(n) {
+  var i = Number(n);
+  if (isNaN(parseFloat(i))) { return false; }
+  if (isNaN(Number(n))) { return false; }
+  if (isFinite(i) === false) { return false; }
+  if (i % 1 != 0) { return false; }
+  return true;
+}
+
+function verifyDoc(res, doc) {
+  if (!doc.title) {
+    return {
+      success: false,
+      message: "Feature must have a title.",
+    };
+  }
+  if (!doc.targetDate) {
+    return {
+      success: false,
+      message: "Feature must have a target date.",
+    };
+  }
+  if (moment(doc.targetDate, "M/D/YYYY", true).isValid() === false) {
+    return {
+      success: false,
+      message: "Target date is not in a recognizable date format. Try MM/DD/YYYY.",
+    };
+  }
+  if (!doc.clientPriority || isValidInt(doc.clientPriority) === false) {
+    return {
+      success: false,
+      message: "Client priority is not an integer.",
+    };
+  }
+  return {
+    success: true,
+  }
+}
+
 router.get('/', function (req, res) {
   Feature.find(function (err, docs) {
-    if (err) { return res.json({success:false}); }
+    if (err) {
+      return res.json({
+        success:false,
+        message:"An unknown error occurred"
+      });
+    }
     if (!docs) { return res.json([]); }
     res.json(docs);
   });
@@ -12,9 +57,17 @@ router.get('/', function (req, res) {
 
 router.post('/', function (req, res) {
   var doc = req.body;
-  console.log(doc);
   Feature.collection.insert(doc, function (err, docs) {
-    if (err) { return res.json({success:false}); }
+    if (err) {
+      return res.json({
+        success:false,
+        message:"An unknown error occurred"
+      });
+    }
+    var verify = verifyDoc(res, body);
+    if (verify.success === false) {
+      return res.json(verify);
+    }
     res.json({success:true});
   });
 });
@@ -22,8 +75,18 @@ router.post('/', function (req, res) {
 router.get('/:id', function (req, res) {
   var id = req.params.id;
   Feature.findOne({"_id":id}, function (err, doc) {
-    if (err) { return res.json({success:false}); }
-    if (!doc) { return res.json({}); }
+    if (err) {
+      return res.json({
+        success:false,
+        message:"An unknown error occurred"
+      });
+    }
+    if (!doc) {
+      return res.json({
+        success:false,
+        message:"A document with that ID does not exist"
+      });
+    }
     res.json(doc);
   });
 });
@@ -32,12 +95,27 @@ router.put('/:id', function (req, res) {
   var id = req.params.id;
   var body = req.body;
   Feature.findById(id, function (err, doc) {
-    if (err) { return res.json({success:false}); }
-    if (!doc) { return res.json({success:false}); }
+    if (err) {
+      return res.json({
+        success:false,
+        message:"An unknown error occurred"
+      });
+    }
+    if (!doc) {
+      return res.json({
+        success:false,
+        message:"A document with that ID does not exist"
+      });
+    }
+    var verify = verifyDoc(res, body);
+    if (verify.success === false) {
+      return res.json(verify);
+    }
     doc.title = body.title;
     doc.description = body.description;
     doc.client = body.client;
-    doc.targetDate = body.targetDate;
+    doc.clientPriority = parseInt(body.clientPriority);
+    doc.targetDate = moment(body.targetDate).utc();
     doc.ticketUrl = body.ticketUrl;
     doc.productArea = body.productArea;
     doc.save(function (err) {
